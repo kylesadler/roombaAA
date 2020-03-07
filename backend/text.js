55df5bee0f02f0baa3b1fb7b50fe0b4555830569
kyle sadler <kylesadler5@gmail.com>
2020-03-07 07:51:37 -0600

    updates

--- backend/text.js
+++ backend/text.js
@@ -5,7 +5,12 @@ const TextSchema = mongoose.Schema({
     text: {
         type: String,
         required: true
+    },
+    created_at: {
+        type: Date,
+        required: true,
+        default: Date.now
     }
-});
+}, { timestamps: true });
 
 const Text = module.exports = mongoose.model('Text', TextSchema);
